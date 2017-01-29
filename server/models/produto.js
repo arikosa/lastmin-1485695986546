5c43ffa4c0e9eec1693131b3f698eaba87db5aa2
arikosa <ariel.kosacoff@gmail.com>
2017-01-29 10:28:03 -0300

    Add files via upload

---
+++ server/models/produto.js
@@ -0,0 +1,17 @@
+'use strict';
+
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+
+    var Produto = new Schema({
+	    nome: String,
+	    descricao: String,
+	    imagen: String,
+	    valor:Number,
+	    ativo: { type: Boolean, 'default' : true },
+	    dataCadastro: { type: Date, 'default' : Date.now },
+	});
+
+module.exports = mongoose.model('Produto', Produto);
+
